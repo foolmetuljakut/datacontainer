b9e86eaf5d712b34df345f432552448cf251f216
foolmetuljakut@TPE15G2 <foolmetuljakut@gmail.com>
2023-06-23 19:47:38 +0200

    fixed the fucking access

--- datacontainer.cpp
+++ datacontainer.cpp
@@ -11,7 +11,10 @@ public:
     typedef std::map<data, DataContainer> dict;
     typedef std::variant<data, list, dict> dctype;
     typedef std::variant<unsigned, std::string> keytype;
+private:
+    dctype container;
 
+public:
     DataContainer(std::string value) : container(value) {}
     DataContainer(std::initializer_list<std::string> list) {
         std::vector<DataContainer> dc;
@@ -31,32 +34,43 @@ public:
         // access the data by this key
         // pass on the rest of the ikey -> rkey
 
-        unsigned i = ikey.find_first_of(separator);
-        std::string nkey = ikey.substr(0, i);
-        std::string rkey = ikey.substr(i + 1);
-
-        std::cout << "access-print: \n" << *this << std::endl;
-
-        switch(container.index()) {
-        case 0: // std::string
+        if(container.index() == 0)
             return std::get<data>(container);
-        case 1: // std::vector
+
+        // index > 0 => we must have some kind of accessor
+        // accessor is numeric => lists
+        // accessor is string => dict
+
+        unsigned i = ikey.find_first_of(separator);
+        std::string nkey, rkey;
+        if(i < (unsigned)(-1)) {
+            nkey = ikey.substr(0, i);
+            rkey = ikey.substr(i + 1);
+        }
+        else { // no separator => last element before value
+            nkey = ikey;
+            rkey = "\0";
+        }
+        
+        // only two remaining options
+        if(container.index() == 1) { // list
             if(ikey.size() == 0)
                 throw "";
-            if(rkey.size() == 0) 
+            if(rkey.size() == 0)
                 throw "not implemented: special case";
-            return std::get<list>(container)[(unsigned)atoi(nkey.c_str())].access(rkey);
-        case 2: // std::map
-            if(!nkey.compare(rkey)) { // special case: end of path and last element is a map itself
-                auto inner = std::get<dict>(container)[nkey];
-                std::cout << inner << std::endl;
-                //wieso entsteht hier eine rekursion????
-                return std::get<data>(inner.container); //std::get<data>(inner);
+            return std::get<list>(container)[(unsigned)atoi(nkey.c_str())].access(rkey, separator); 
+        } else { //dict
+            dict d = std::get<dict>(container);
+            if(d.find(nkey) == d.end())
+                throw "gibt's nicht! kauf ich auch nicht!";
+            if(!rkey.compare("\0")) { // reached end of key - if this isn't a value we don't have enough keys
+                auto lastc = d[nkey].container;
+                if(lastc.index() > 0)
+                    throw "accessed element is not a value. key is to short to go all the way down.";
+                return std::get<data>(lastc);
             }
-            return std::get<dict>(container)[nkey].access(nkey.compare(rkey) ? rkey : "");
-        default: // ???
-            throw "the fuck did you even do?!?";
-        }
+            return std::get<dict>(container)[nkey].access(rkey, separator); 
+        } 
     }
 
     void set(std::string key, std::string value) {
@@ -68,26 +82,23 @@ public:
             std::get<dict>(container)[key] = value;
     }
     friend std::ostream& operator<<(std::ostream& out, DataContainer& dc);
-private:
-    dctype container;
 };
 
 std::ostream& operator<<(std::ostream& out, DataContainer& dc) {
     auto container = dc.container;
-    out << "container type: ";
     switch(container.index()) {
         case 0: // std::string
-            return out << "data, " << std::get<DataContainer::data>(container);
+            return out << std::get<DataContainer::data>(container);
         case 1: // std::vector
-            out << "list\n";
+            out << "[\n";
             for(auto& item : std::get<DataContainer::list>(container))
                 out << item << std::endl;
-            return out;
+            return out << "]";
         case 2: // std::map
-            out << "dict\n";
+            out << "{\n";
             for(auto& kvpair : std::get<DataContainer::dict>(container))
                 out << kvpair.first << ": " << kvpair.second << std::endl;
-            return out;
+            return out << "}";
         default: // ???
             throw "the fuck did you even do?!?";
         }
@@ -95,12 +106,18 @@ std::ostream& operator<<(std::ostream& out, DataContainer& dc) {
 
 int main(int argc, char **argv) {
 
-    // DataContainer sample("value");
-    DataContainer sample;
+    DataContainer sample("value");
+    std::cout << "sample with just value:\n" << sample << std::endl << "-----------------" << std::endl;
+
+    sample = DataContainer();
     sample.set("key", "value");
     sample.set("key2", "value");
+    std::cout << "sample with 2 key value pairs:\n"  << sample << std::endl << "-----------------" << std::endl;
+
     DataContainer containingSample;
     containingSample.set("outerkey", sample);
-    std::cout << sample.access("outerkey.key2") << std::endl;
+    std::cout << "sample with sub sample:\n"  << containingSample << std::endl << "-----------------" << std::endl;
+
+    std::cout << "whats under the first key \"outerkey.key2\":\n"  << containingSample.access("outerkey.key2") << std::endl << "-----------------" << std::endl;
     return 0;
 }
